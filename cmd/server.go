245ac9276e515e1a2dda4e710e076eeda3cf8ef2
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-09-01 02:32:19 +0900

    add: User query

--- cmd/server.go
+++ cmd/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"github.com/nsym-m/go-graphql/internal/graph"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/nsym-m/go-graphql/internal/graph"
+	"github.com/nsym-m/go-graphql/internal/service"
 )
 
-const defaultPort = "8080"
+const (
+	defaultPort = "8080"
+	dbFile      = "./db/mygraphql.db"
+)
 
 func main() {
 	port := os.Getenv("PORT")
@@ -18,7 +25,15 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	db, err := sql.Open("sqlite3", fmt.Sprintf("%s?_foreign_keys=on", dbFile))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
+		Srv: service.New(db),
+	}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/99designs/gqlgen v0.17.49
 	github.com/friendsofgo/errors v0.9.2
+	github.com/mattn/go-sqlite3 v1.14.14
 	github.com/vektah/gqlparser/v2 v2.5.16
 	github.com/volatiletech/null/v8 v8.1.2
 	github.com/volatiletech/sqlboiler v3.7.1+incompatible
