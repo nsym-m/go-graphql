7474f8ebe7e3754f6d9d5a4bd6f4695a2fe61b60
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-09-23 18:29:39 +0900

    add: node

--- internal/graph/query.resolvers.go
+++ internal/graph/query.resolvers.go
@@ -7,7 +7,9 @@ package graph
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/friendsofgo/errors"
 	"github.com/nsym-m/go-graphql/internal/graph/model"
 )
 
@@ -28,7 +30,23 @@ func (r *queryResolver) User(ctx context.Context, name string) (*model.User, err
 
 // Node is the resolver for the node field.
 func (r *queryResolver) Node(ctx context.Context, id string) (model.Node, error) {
-	panic(fmt.Errorf("not implemented: Node - node"))
+	nElems := strings.SplitN(id, "_", 2)
+	nType, _ := nElems[0], nElems[1]
+
+	switch nType {
+	case "U":
+		return r.Srv.GetUserByID(ctx, id)
+	case "REPO":
+		return r.Srv.GetRepoByID(ctx, id)
+	case "ISSUE":
+		return r.Srv.GetIssueByID(ctx, id)
+	case "PJ":
+		return r.Srv.GetProjectByID(ctx, id)
+	case "PR":
+		return r.Srv.GetPullRequestByID(ctx, id)
+	default:
+		return nil, errors.New("invalid ID")
+	}
 }
 
 // Mutation returns MutationResolver implementation.
